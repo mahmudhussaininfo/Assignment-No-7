54c2b6f240782c3df50dc9fbee0da3eee7c89c95
Mahmud <77590197+mahmudhussaininfo@users.noreply.github.com>
2022-04-27 15:51:34 +0600

    My Assignment No 7

---
+++ app.js
@@ -0,0 +1,631 @@
+
+
+const devs = [
+
+    {
+        Name : 'Mahmud',
+        Roll : 1,
+        Age : 25,
+        Clas : 11,
+        Gender : 'Male',
+        Location : 'Sylhet',
+        AdmissionFee : 5000,
+
+    },
+    {
+        Name : 'Kulsuma',
+        Roll : 2,
+        Age : 25,
+        Clas : 11,
+        Gender : 'Female',
+        Location : 'Sylhet',
+        AdmissionFee : 5000,
+
+    },
+    {
+        Name : 'Kuddus',
+        Roll : 3,
+        Age : 24,
+        Clas : 10,
+        Gender : 'Male',
+        Location : 'Dhaka',
+        AdmissionFee : 4000,
+
+    },
+    {
+        Name : 'Sokina',
+        Roll : 4,
+        Age : 23,
+        Clas : 10,
+        Gender : 'Female',
+        Location : 'Dhaka',
+        AdmissionFee : 4000,
+
+    },
+    {
+        Name : 'Borkot',
+        Roll : 5,
+        Age : 20,
+        Clas : 9,
+        Gender : 'Male',
+        Location : 'Feni',
+        AdmissionFee : 8000,
+
+    },
+    {
+        Name : 'Bilkis',
+        Roll : 6,
+        Age : 20,
+        Clas : 9,
+        Gender : 'Female',
+        Location : 'Feni',
+        AdmissionFee : 8000,
+
+    },
+    {
+        Name : 'Mokles',
+        Roll : 7,
+        Age : 15,
+        Clas : 8,
+        Gender : 'Male',
+        Location : 'Borishal',
+        AdmissionFee : 6000,
+
+    },
+    {
+        Name : 'Moni',
+        Roll : 8,
+        Age : 16,
+        Clas : 8,
+        Gender : 'Female',
+        Location : 'Borishal',
+        AdmissionFee : 6000,
+
+    },
+    {
+        Name : 'Rabbani',
+        Roll : 9,
+        Age : 21,
+        Clas : 7,
+        Gender : 'Male',
+        Location : 'Bhula',
+        AdmissionFee : 2000,
+
+    },
+    {
+        Name : 'Rasheda',
+        Roll : 10,
+        Age : 21,
+        Clas : 7,
+        Gender : 'Female',
+        Location : 'Bhula',
+        AdmissionFee : 2000,
+
+    },
+    {
+        Name : 'Elias',
+        Roll : 11,
+        Age : 11,
+        Clas : 6,
+        Gender : 'Male',
+        Location : 'Coxbazar',
+        AdmissionFee : 1000,
+
+    },
+    {
+        Name : 'Eti',
+        Roll : 12,
+        Age : 11,
+        Clas : 6,
+        Gender : 'Female',
+        Location : 'Coxbazar',
+        AdmissionFee : 1000,
+
+    },
+    {
+        Name : 'Rahim',
+        Roll : 13,
+        Age : 12,
+        Clas : 5,
+        Gender : 'Male',
+        Location : 'Rajshahi',
+        AdmissionFee : 3000,
+
+    },
+    {
+        Name : 'Rahima',
+        Roll : 14,
+        Age : 12,
+        Clas : 5,
+        Gender : 'Female',
+        Location : 'Rajshahi',
+        AdmissionFee : 3000,
+
+    },
+    {
+        Name : 'Lipu',
+        Roll : 15,
+        Age : 10,
+        Clas : 4,
+        Gender : 'Male',
+        Location : 'Habiganj',
+        AdmissionFee : 7000,
+
+    },
+    {
+        Name : 'Lipy',
+        Roll : 16,
+        Age : 10,
+        Clas : 4,
+        Gender : 'Female',
+        Location : 'Habiganj',
+        AdmissionFee : 7000,
+
+    },
+    {
+        Name : 'Monsur',
+        Roll : 17,
+        Age : 13,
+        Clas : 3,
+        Gender : 'Male',
+        Location : 'Chittagong',
+        AdmissionFee : 8000,
+
+    },
+    {
+        Name : 'Muna',
+        Roll : 18,
+        Age : 13,
+        Clas : 3,
+        Gender : 'Female',
+        Location : 'Chittagong',
+        AdmissionFee : 8000,
+
+    },
+    {
+        Name : 'Redwan',
+        Roll : 19,
+        Age : 14,
+        Clas : 2,
+        Gender : 'Male',
+        Location : 'Saintmartin',
+        AdmissionFee : 9000,
+
+    },
+    {
+        Name : 'Rimi',
+        Roll : 20,
+        Age : 14,
+        Clas : 2,
+        Gender : 'Female',
+        Location : 'Saintmartin',
+        AdmissionFee : 9000,
+
+    },
+    {
+        Name : 'Kawsar',
+        Roll : 21,
+        Age : 5,
+        Clas : 1,
+        Gender : 'Male',
+        Location : 'Rangamati',
+        AdmissionFee : 3000,
+
+    },
+    {
+        Name : 'kashifa',
+        Roll : 22,
+        Age : 5,
+        Clas : 1,
+        Gender : 'Female',
+        Location : 'Rangamati',
+        AdmissionFee : 3000,
+
+    },
+    {
+        Name : 'Kabila',
+        Roll : 23,
+        Age : 19,
+        Clas : 10,
+        Gender : 'Male',
+        Location : 'Nuwakali',
+        AdmissionFee : 10000,
+
+    },
+    {
+        Name : 'Rokiya',
+        Roll : 24,
+        Age : 19,
+        Clas : 10,
+        Gender : 'Female',
+        Location : 'Nuwakali',
+        AdmissionFee : 10000,
+
+    },
+    {
+        Name : 'Anas',
+        Roll : 25,
+        Age : 20,
+        Clas : 11,
+        Gender : 'Male',
+        Location : 'Zakigonj',
+        AdmissionFee : 12000,
+
+    },
+    {
+        Name : 'Masuma',
+        Roll : 26,
+        Age : 20,
+        Clas : 11,
+        Gender : 'Female',
+        Location : 'Zakigonj',
+        AdmissionFee : 12000,
+
+    },
+    {
+        Name : 'Fateha',
+        Roll : 27,
+        Age : 24,
+        Clas : 9,
+        Gender : 'Female',
+        Location : 'Kanaighat',
+        AdmissionFee : 1000,
+
+    },
+    {
+        Name : 'Shuhag',
+        Roll : 28,
+        Age : 24,
+        Clas : 9,
+        Gender : 'Male',
+        Location : 'Kanaighat',
+        AdmissionFee : 1000,
+
+    },
+    {
+        Name : 'Shipu',
+        Roll : 29,
+        Age : 24,
+        Clas : 6,
+        Gender : 'Male',
+        Location : 'Cumilla',
+        AdmissionFee : 4000,
+
+    },
+    {
+        Name : 'Shima',
+        Roll : 30,
+        Age : 24,
+        Clas : 6,
+        Gender : 'Female',
+        Location : 'Cumilla',
+        AdmissionFee : 4000,
+
+    },
+
+    
+
+];
+
+// const devs = [
+
+//     {
+//         Name : 'Mahmud',
+//         age : 20,
+//         Skill : 'Ios',
+
+//     },
+//     {
+//         Name : 'Mahmuda',
+//         age : 21,
+//         Skill : 'Iosd',
+
+//     },
+
+// ];
+
+let fee = 0;
+devs.map((data, index) => {
+
+    // console.log(data);
+
+
+    // - Find All Female Students
+
+    // if(data.Gender == 'Female'){
+
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}  
+        
+        
+        
+        
+    //     `); 
+    // };
+
+    /**
+     * Find class wise student result
+     */
+
+    // if(data.Clas == 1){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 2){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 3){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 4){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 5){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 6){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 7){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 8){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 9 ){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 10){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+    // if(data.Clas == 11){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+    //     `); 
+    // };
+
+
+    /**
+     *  Location wise student result
+     */
+
+    // if(data.Location = 'Dhaka'){
+    //         console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Gender : ${data.Gender}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+        
+    //     `); 
+    // };
+    
+    // if(data.Location = 'Sylhet'){
+    //         console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Gender : ${data.Gender}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+        
+    //     `); 
+    // };
+    
+    // if(data.Location = 'Chittagong'){
+    //         console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Gender : ${data.Gender}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+        
+    //     `); 
+    // };
+    
+    // if(data.Location = 'Borishal'){
+    //         console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Gender : ${data.Gender}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+        
+    //     `); 
+    // };
+    
+    // if(data.Location = 'Nuwakali'){
+    //         console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Gender : ${data.Gender}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+        
+    //     `); 
+    // };
+    
+    // if(data.Location = 'Rajshahi'){
+        //     console.log(` 
+        
+        
+        // Name:  ${data.Name}
+        // Gender : ${data.Gender}
+        // Roll : ${data.Roll}  
+        // Age : ${data.Age}  
+        // Class : ${data.Clas}   
+        // Location : ${data.Location}  
+        // Fees : ${data.AdmissionFee}          
+        
+        // `); 
+    // };
+
+
+    /**
+     * find student between 10 - 25 age
+     */
+
+    // if(data.Age >= 10 && data.Age <= 25){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Gender : ${data.Gender}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+        
+    //     `); 
+    // };
+    
+    
+
+
+    
+    
+    
+    fee += data.AdmissionFee;
+});
+
+
+// Total Admission Fees
+console.log(` 
+
+===============================================================
+Total Admission Fees : ${fee} 
+
+`);
+
+
+
+
+
+
+
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+if(data.Age >= 10 && data.Age <= 25){
+    //     console.log(` 
+        
+        
+    //     Name:  ${data.Name}
+    //     Gender : ${data.Gender}
+    //     Roll : ${data.Roll}  
+    //     Age : ${data.Age}  
+    //     Class : ${data.Clas}   
+    //     Location : ${data.Location}  
+    //     Fees : ${data.AdmissionFee}          
+        
+    //     `); 
+    // };
